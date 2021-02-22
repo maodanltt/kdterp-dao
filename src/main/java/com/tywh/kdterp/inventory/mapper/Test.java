3a79b7e664357871ff338be3ca25fd5af383d423
maodan <maodan@163.com>
2021-02-22 18:13:35 +0800

    更新dao

--- src/main/java/com/tywh/kdterp/inventory/mapper/Test.java
+++ src/main/java/com/tywh/kdterp/inventory/mapper/Test.java
@@ -1,12 +1,30 @@
 package com.tywh.kdterp.inventory.mapper;
 
+import com.tywh.kdterp.pojo.Condition;
+import com.tywh.kdterp.pojo.Item;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionException;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 public class Test {
 
     public static void main(String[] args) {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
+        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:spring/applicationContext.xml");
+        SqlSessionFactory sqlSessionFactory  = (SqlSessionFactory)ac.getBean("sqlSessionFactory");
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Condition condition = new Condition();
+        condition.setEnddate("2021-02");
+        condition.setStartdate("2021-02");
+        try {
+            List<Item> list = sqlSession.selectList("queryItemList", Condition.class);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
         System.out.println("ac");
     }
 }
